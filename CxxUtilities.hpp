5249e0842b97a4944fb925a1b104b4a5d48a0c3e
hogliux <fabian.renn@gmail.com>
2025-02-04 14:26:50 +0100

    Updated CxxUtilities from upstream

--- CxxUtilities.hpp
+++ CxxUtilities.hpp
@@ -175,7 +175,7 @@ public:
 
     reverse_lock(const reverse_lock&) = delete;
     reverse_lock& operator=(const reverse_lock&) = delete;
-    reverse_lock(reverse_lock&& other) noexcept : lock(other.lock), unlocked(other.unlocked) { other.unlocked_= false; }
+    reverse_lock(reverse_lock&& other) noexcept : lock(other.lock), unlocked(other.unlocked) { other.unlocked = false; }
 
     reverse_lock& operator=(reverse_lock&& other) noexcept
     {
@@ -204,14 +204,17 @@ private:
 //====================================================================
 template<typename T>
 constexpr std::enable_if_t<std::is_integral_v<T>, T> byteswap(T value) noexcept {
-   #if defined(__cpp_lib_byteswap) && __cplusplus >= __cpp_lib_byteswap
+   #if defined(cpp_lib_byteswap) && cplusplus >= __cpp_lib_byteswap
     return std::byteswap<T>(value);
    #else
     static_assert(std::has_unique_object_representations_v<T>,  "T may not have padding bits");
     auto byte_representation = std::bit_cast<std::array<std::byte, sizeof(T)>>(value);
     auto const reverse_byte_representation = std::invoke([&byte_representation] () {
         std::array<std::byte, sizeof(T)> result;
-        std::copy(byte_representation.rbegin(), byte_representation.rend(), result.begin());
+
+        for (std::size_t i = 0; i < sizeof(T); ++i)
+            result[sizeof(T)-i-1] = byte_representation[i];
+
         return result;
     });
     return std::bit_cast<T>(reverse_byte_representation);
